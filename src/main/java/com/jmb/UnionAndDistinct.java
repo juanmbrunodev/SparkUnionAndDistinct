f0115a7ebb9fc047326161606337b8df3b4d0034
juanbruno99 <juan.marcos.bruno@gmail.com>
2021-08-10 16:01:03 +0200

    Completing transformations, changes in pom.xml file to use Spark 3.0.0 dependency version instead of preview one

--- src/main/java/com/jmb/UnionAndDistinct.java
+++ src/main/java/com/jmb/UnionAndDistinct.java
@@ -4,6 +4,8 @@ import org.apache.spark.sql.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.apache.spark.sql.functions.lit;
+
 
 public class UnionAndDistinct {
 
@@ -26,7 +28,7 @@ public class UnionAndDistinct {
                 .appName("UnionAndDistinct")
                 .master("local").getOrCreate();
 
-        //Ingest data from CSV file into a DataFrame
+        //Ingest data from CSV files into a DataFrames
         Dataset<Row> dfSalesUs = session.read()
                 .format(SPARK_FILES_FORMAT)
                 .option("header", "true")
@@ -40,8 +42,38 @@ public class UnionAndDistinct {
                 .load(PATH_RESOURCES_DF2);
 
         //Print the 5 first records to inspect both the schema and data of the loaded DataFrames
+        LOGGER.info("US Salesmen DataFrame Ingested: ");
         dfSalesUs.show();
+        LOGGER.info("UK Salesmen DataFrame Ingested: ");
         dfSalesUk.show();
+
+        //Inspect both dfs schemas to make sure the structure matches thus allowing a Union Transformation
+        dfSalesUs.printSchema();
+        dfSalesUk.printSchema();
+
+        //Perform a Union of the DataFrames ingested
+        Dataset<Row> allEmployees = dfSalesUs.union(dfSalesUk);
+
+        //Print resulting merged DataFrame
+        allEmployees.show();
+
+        //Rearrange columns order.
+        Dataset<Row> dfUkSalesReOrg = dfSalesUk.drop("department").withColumn("department", lit("TBD"));
+
+        dfUkSalesReOrg.show(2);
+
+        //Now attempt a unionByName - union based on matching columns regardless of the schemas
+        //Perform a Union of the DataFrames ingested
+        Dataset<Row> allEmployeesUnionByName = dfSalesUs.unionByName(dfUkSalesReOrg);
+
+        // Print results
+        allEmployeesUnionByName.show();
+
+        //Drop duplicates based on employee name and department as identity criteria
+        Dataset<Row> uniqueAllEmployees = allEmployees.dropDuplicates("employee_name", "department");
+
+        //Print results
+        uniqueAllEmployees.show();
 
     }
 
